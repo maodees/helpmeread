99f824e60c04286cec069b46af08728708ed81ae
maodees <maodees@users.noreply.github.com>
2025-02-12 15:01:11 +0000

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+#import pdf2image as pfimg
+import pytesseract as tess
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+print ("hello world")
