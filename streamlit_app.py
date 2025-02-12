b64afb206f2884fc35f259ef3d85ef21f4c5162d
maodees <maodees@users.noreply.github.com>
2025-02-12 15:14:44 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,5 +9,5 @@ st.write(
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
-str_text = open("sample\sample1.txt").read()
+str_text = open('sample\sample1.txt').read()
 st.write(str_text)
